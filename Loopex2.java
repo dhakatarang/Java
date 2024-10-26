25cbe50c3992dce6e2ad0b43c2cc8eb24a5d64e6
dhakatarang <tarang.kumar.cse.2022@miet.ac.in>
2024-10-26 19:24:28 +0530

    java practice

---
+++ Loopex2.java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner;
+public class Loopex2 {
+    public static void main(String[] args) {
+    //program to find whether a letter is uppercase or lower case
+        
+    // Scanner input=new Scanner(System.in);
+    // int a=input.nextInt();
+    // int b=input.nextInt();
+    // int c=input.nextInt();
+
+    // if(a>b && a>c){
+    //     System.out.println("a is the largest no.");
+    // }
+    // else if(b>c){
+    //     System.out.println("b is the largest no.");
+    // }
+    // else{
+    //     System.out.println("c is the largest no.");
+    // }
+
+    //prgram to find upper case or lower case letter
+    Scanner input=new Scanner(System.in);
+    int c=input.next().trim().charAt(0);
+    if (c>='a' && c<='z'){
+        System.out.println("The letter is lower case");
+    }
+    else{
+        System.out.println("The letter is upper case");
+    }
+
+    
+    }
+}
+
