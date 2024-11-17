93d86121e0b55bf8b266c9c6cef2b156aad1cb6a
dhakatarang <tarang.kumar.cse.2022@miet.ac.in>
2024-11-17 22:09:58 +0530

    array input example

--- Javabasics/Array/Array.java
+++ Javabasics/Array/Array.java
@@ -4,8 +4,11 @@ public class Array{
 public static void main(String[] args) {
     int[] arr= {1,2,34};
     
+    for(int i=0;i<arr.length;i++){
+        System.out.println(arr[i]);
 
+    }
      
-    System.out.print(arr);
+    
 }
 }
