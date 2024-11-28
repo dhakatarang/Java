37d04547512206857c16daf78c701de3ab600c21
dhakatarang <tarang.kumar.cse.2022@miet.ac.in>
2024-11-28 18:07:17 +0530

    vasic array program

--- Javabasics/Array/Array.java
+++ Javabasics/Array/Array.java
@@ -1,13 +1,38 @@
 package Javabasics.Array;
-//import java.util.Scanner;
+import java.util.Scanner;
 public class Array{
-public static void main(String[] args) {
-    int[] arr= {1,2,34};
+// public static void main(String[] args) {
+    // basic array example
+    // int[] arr= {1,2,34};
     
-    for(int i=0;i<arr.length;i++){
-        System.out.println(arr[i]);
+    // for(int i=0;i<arr.length;i++){
+    //     System.out.println(arr[i]);}
+
+
+    //taking array as an input from user
+    // Scanner in=new Scanner(System.in);
+    // int val= in.nextInt();
+    // for(int i=0;i<val.length;i++){
+    //     int arr[i]=
+    class Main {
+        public static void main(String[] args) {
+         
+          // create an array
+          int[] age = {12, 4, 5, 2, 5};
+       
+          // access each array elements
+          System.out.println("Accessing Elements of Array:");
+          System.out.println("First Element: " + age[0]);
+          System.out.println("Second Element: " + age[1]);
+          System.out.println("Third Element: " + age[2]);
+          System.out.println("Fourth Element: " + age[3]);
+          System.out.println("Fifth Element: " + age[4]);
+        }
+       }
 
     }
+
+
      
     
 }
